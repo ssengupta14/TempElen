3c0002e5a415cf5eff3d1ca41e7cece1c6f2bfd0
souravceo <ssengupta@paypal.com>
2014-07-14 17:47:40 -0400

    adding DPOs

---
+++ elenverve-web/src/main/java/com/elenverve/dpo/pages/Home.java
@@ -0,0 +1,22 @@
+package com.elenverve.dpo.pages;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.elenverve.dpo.MenuDpo;
+import com.elenverve.dynamic.TestMenu;
+
+public class Home {
+	
+	private List<MenuDpo> menus = new LinkedList<MenuDpo>();
+
+	public List<MenuDpo> getMenus() {
+		TestMenu men = new TestMenu();
+		return men.getMenus();
+	}
+	
+	
+	
+
+
+}
