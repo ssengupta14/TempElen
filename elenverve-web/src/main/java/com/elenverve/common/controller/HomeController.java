c40f69741fdbff66d4be2c1a4fef6581fb3d9a21
sourav <ssengupt2@gmail.com>
2014-07-15 21:59:55 -0400

    added import

--- elenverve-web/src/main/java/com/elenverve/common/controller/HomeController.java
+++ elenverve-web/src/main/java/com/elenverve/common/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.elenverve.dpo.pages.Home;
 import com.elenverve.model.Product;
 import com.elenverve.parser.ProductParser;
 
